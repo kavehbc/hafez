5cfa1422525be342ec068c9b69f58c2ac9aee240
Kaveh Bakhtiyari <kavehbc@yahoo.com>
2023-04-25 20:24:46 -0400

    data structure ideation

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Hafez Poems & Omen
 
 This library is under-development and will be released soon.
+
+Poems data structure:
+
+```json
+{"id": 1,
+"poem": [],
+"meaning": "",
+"vocal": "https://..."}
+```
---
+++ examples/example.py
@@ -0,0 +1,32 @@
+import hafez
+
+def main():
+    """
+
+    {"id": 1,
+    "poem": [],
+    "meaning": "",
+    "vocal": "https://..."}
+
+    :return:
+    """
+
+    # it returns the total number of poems
+    # returns: int
+    total_number_of_poems = hafez.total_poems()
+
+    # get a poem by ID
+    # returns: dict
+    poem_5 = hafez.get_poem(5)
+
+    # get omen (fal)
+    # returns: dict
+    omen = hafez.omen() # same as: hafez.fal()
+
+    # search within the verses of poems
+    # returns: list[dict]
+    search_result = hafez.search("حافظ")
+
+
+if __name__ == '__main__':
+    main()
