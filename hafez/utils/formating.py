e0b716863a936c2636faa6390aa10277399c12ab
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2024-03-13 00:23:37 -0400

    updated dataset

--- hafez/utils/formating.py
+++ hafez/utils/formating.py
@@ -24,3 +24,15 @@ def df_to_dict(df):
                     "mp3": f"https://de.loveziba.com/2019/10/{mp3_id_formatting(poem_id)}.mp3"}
         lst_poems.append(dic_poem)
     return lst_poems
+
+
+def convert_arabic_to_persian(verse):
+    arabic = ['ي', 'ك', 'ة']
+    farsi = ['ی', 'ک', 'ه']
+
+    for index, item in enumerate(arabic):
+        verse = verse.replace(item, farsi[index])
+        
+    # replacing persian/farsi half-space with regular space
+    verse = verse.replace(u"\u200c", " ")
+    return verse
